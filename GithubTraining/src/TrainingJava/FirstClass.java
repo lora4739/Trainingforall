0bfba2ed8150a99566b034c2e6e403bf934f0a4f
lora4739 <lora5780@gmail.com>
2020-05-16 10:11:33 -0400

    commiting the first class

---
+++ GithubTraining/src/TrainingJava/FirstClass.java
@@ -0,0 +1,11 @@
+package TrainingJava;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" I am learning Git");
+	}
+
+}
